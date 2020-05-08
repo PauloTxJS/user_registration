5a753a5064ec416e8613d10b6ea0d73c570b3bf7
Paulo Teixeira <paulo.teixeira541@gmail.com>
2020-05-08 15:44:11 -0300

    Terminei a exclusão dos dados.

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -238,7 +238,7 @@ class UserController {
             <td>${Utils.dateFormat(dataUser.register)}</td>
             <td>
                 <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
-                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                <button type="button" class="btn btn-danger btn-delete btn-xs btn-flat">Excluir</button>
             </td>
             
         `;
@@ -253,7 +253,19 @@ class UserController {
 
     addEventsTr(tr) {
 
-        tr.querySelector(".btn-edit").addEventListener('click', e => {
+        tr.querySelector(".btn-delete").addEventListener('click', e => {
+
+            if (confirm("Deseja realmente excluir?")) {
+
+                tr.remove();
+                
+                this.updateCount();
+
+            }
+
+         });
+
+        tr.querySelector(".btn-edit").addEventListener('click', e => {    
 
             let json = JSON.parse(tr.dataset.user);
             
